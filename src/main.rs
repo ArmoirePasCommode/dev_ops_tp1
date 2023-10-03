56b5f00ea5b3d3632605252b809a8fa8cdcc0c99
eliot guihal <eliot.guihal@ynov.com>
2023-10-03 15:23:04 +0100

    20/20

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,49 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpResponse, HttpServer, Result};
+use env_logger::Env;
 use serde::Serialize;
 
 #[derive(Serialize)]
-struct HeadersResponse {
+struct RequestHeaders {
     headers: Vec<(String, String)>,
 }
 
-#[get("/ping")]
 async fn ping(request: actix_web::HttpRequest) -> HttpResponse {
-    let headers_map: Vec<(String, String)> = request
-        .headers()
+    let headers = request.headers();
+    let headers_vec: Vec<(String, String)> = headers
         .iter()
-        .map(|header| {
-            let name = header.0.as_str().to_string();
-            let value = header.1.to_str().unwrap_or("").to_string();
-            (name, value)
+        .map(|(name, value)| {
+            (
+                name.as_str().to_string(),
+                value.to_str().unwrap_or("").to_string(),
+            )
         })
         .collect();
 
-    let response = HeadersResponse {
-        headers: headers_map,
+    let response_data = RequestHeaders {
+        headers: headers_vec,
     };
 
-    HttpResponse::Ok().json(response)
+    HttpResponse::Ok().json(response_data)
+}
+
+async fn not_found() -> HttpResponse {
+    HttpResponse::NotFound().finish()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(web::scope("/").service(ping)))
-        .bind("127.0.0.1:7878")?
-        .run()
-        .await
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    let listen_port = std::env::var("PING_LISTEN_PORT")
+        .and_then(|s| s.parse().ok())
+        .unwrap_or(7878);
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/ping", web::get().to(ping))
+            .default_service(web::route().to(not_found)) // Gère toutes les autres routes
+    })
+    .bind(("127.0.0.1", listen_port))?
+    .run()
+    .await
 }
